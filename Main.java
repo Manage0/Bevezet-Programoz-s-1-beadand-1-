9d1d253b4d5ff2e3fd8dbc69051ddf92fccdee92
Manage0 <61554465+Manage0@users.noreply.github.com>
2020-04-30 08:07:43 +0200

    Second Problem Solved
    1,99$/pounds

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	    buyingThreeForADollar(4);
         buyingThreeForADollar(5);
+        buyingOnePointNinetyNineDollarPerPound(false, 4);
     }
 
     private static void buyingThreeForADollar(int amount){
@@ -12,4 +13,16 @@ public class Main {
         System.out.println("You've bought "+amount+" of something, that has a price of three for a dollar, so the " +
                 "final price is "+ (((double)((int)(price*amount*100)))/100)+".");
     }
+
+    private static void buyingOnePointNinetyNineDollarPerPound(boolean pounds, int amount){
+        if (pounds){
+            System.out.println("You've bought "+amount+" pounds of something, that has a price of 1.99$/pounds, so the " +
+                    "final price is "+ OnePointNinetyNineDollarPerPound.getPrice()*amount+"$.");
+        }
+        if (!pounds){
+            System.out.println("You've bought "+amount+" ounces of something, that has a price of 1.99$/pounds, so the " +
+                    "final price is "+ OnePointNinetyNineDollarPerPound.getPrice()*((double)amount/16)+"$.");
+        }
+    }
+
 }
---
+++ OnePointNinetyNineDollarPerPound.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class OnePointNinetyNineDollarPerPound {
+    static private double price=1.99;
+
+    public static double getPrice() {
+        return price;
+    }
+}
