ffd155eda08731a32f93301d3586c1c858c6155c
Manage0 <61554465+Manage0@users.noreply.github.com>
2020-04-30 09:19:44 +0200

    Third problem solved
    Buy two get one free solved and the first two solution is fixed.

---
+++ BuyTwoGetOneFree.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class BuyTwoGetOneFree {
+    private static int price=1;
+
+    public static int getPrice() {
+        return price;
+    }
+}
--- Main.java
+++ Main.java
@@ -6,23 +6,31 @@ public class Main {
 	    buyingThreeForADollar(4);
         buyingThreeForADollar(5);
         buyingOnePointNinetyNineDollarPerPound(false, 4);
+        buyingBuyTwoGetOneFree(3);
     }
 
     private static void buyingThreeForADollar(int amount){
         double price=((double)ThreeForADollar.getPrice()/3);
         System.out.println("You've bought "+amount+" of something, that has a price of three for a dollar, so the " +
-                "final price is "+ (((double)((int)(price*amount*100)))/100)+".");
+                "final price is "+ (double)(Math.round(price*amount*100))/100+"$.");
     }
 
     private static void buyingOnePointNinetyNineDollarPerPound(boolean pounds, int amount){
+        double price=OnePointNinetyNineDollarPerPound.getPrice();
         if (pounds){
             System.out.println("You've bought "+amount+" pounds of something, that has a price of 1.99$/pounds, so the " +
-                    "final price is "+ OnePointNinetyNineDollarPerPound.getPrice()*amount+"$.");
+                    "final price is "+(double)(Math.round(price*amount*100))/100 +"$.");
         }
         if (!pounds){
             System.out.println("You've bought "+amount+" ounces of something, that has a price of 1.99$/pounds, so the " +
-                    "final price is "+ OnePointNinetyNineDollarPerPound.getPrice()*((double)amount/16)+"$.");
+                    "final price is "+(double)(Math.round(price*amount/16*100))/100+"$.");
         }
+    }
+
+    private static void buyingBuyTwoGetOneFree(double amount){
+        double price = Math.round(amount/2)*BuyTwoGetOneFree.getPrice();
+        System.out.println("You've bought "+(int)amount+" of something, and every second one is free so the " +
+                "final price is "+(int)price +"$.");
     }
 
 }
